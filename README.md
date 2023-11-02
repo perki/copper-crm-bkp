7285554b9ab6c5f59be34ad1b2151400886be11c
Perki <perki@pryv.com>
2023-11-02 11:23:12 +0100

    Added call rate limiting

--- README.md
+++ README.md
@@ -12,14 +12,16 @@ you need a Copper CRM API KEY  / Copper CRM account email pair
 
 `COPPER_APIKEY=xxxxxxx COPPER_EMAIL=you@email.xyz node index.js`
 
-It's possible for multiple reasons that the backup fails or misses items. 
-It is safe to re-run it several time, already fetched items will be skipped.
+It's possible for multiple reasons (timeouts, over rate limit) that the backup fails or misses items. 
+It is safe to re-run it several times, already fetched items will be skipped.
 
 ## Notes 
 
 This backup is not incremental it first fetches list of the different items then retrieve them one by one. 
 Data is saved in "data/" folder in `json` format.
 
+Some attempts of parallelization have been made, but they ended up in rate-limiting Errors from Copper's API which is limiting to 180 calls / minutes.
+This is why this code is not optimitzed and uses intensively fs.Sync methods and executing calls in series.
+
 ### Missing 
 - Files
-- Related Items
--- index.js
+++ index.js
@@ -14,12 +14,70 @@ const HEADERS = {
   'Content-Type': 'application/json'
  };
 
+ const entityMap = {
+  'companies': 'company',
+  'people': 'person',
+  'opportunities': 'opportunity',
+  'projects': 'project',
+  'tasks': 'task',
+  'leads': 'lead'
+};
+
+(async () => {
+  for (const item of [
+    'account', 'lead_statuses', 'contact_types', 'customer_sources', 
+    'loss_reasons', 'pipelines', 'pipeline_stages', 'tags', 
+    'activity_types', 'custom_activity_types', 'custom_field_definitions'
+  ]) {
+    await getSave(item);
+  }
+  for (const item of Object.keys(entityMap)) {
+    await getItem(item);
+  }
+  for (const item of Object.keys(entityMap)) {
+    await getActivities(item);
+  }
+  for (const item of Object.keys(entityMap)) {
+    await getRelated(item, 'related');
+    await getRelated(item, 'files');
+  }
+})();
+
+let callsCount = 0;
+let waitBetweenCalls = 0;
+const checkRateMs = 10*1000;
+const maxCallsMinute = 30;
+const expectedCallTimeMSec = 60 * 1000 / maxCallsMinute;
+/**
+ * Check rate is called every 10 seconds, it avoids being over the 180 calls/minutes
+ */
+async function checkRate() {
+  const actualCallTimeMSec = checkRateMs / (callsCount + 0.0001);
+  waitBetweenCalls += expectedCallTimeMSec - actualCallTimeMSec;
+  console.log('Actual callTime: ' + actualCallTimeMSec + ' expected: ' + expectedCallTimeMSec);
+  if (waitBetweenCalls < 0) waitBetweenCalls = 0;
+  if (waitBetweenCalls > expectedCallTimeMSec) waitBetweenCalls = expectedCallTimeMSec;
+  const actualCallrateMin = (60 * 1000 / checkRateMs) * callsCount;
+  console.log('Call Rate: ' + actualCallrateMin + ' wait: ' + waitBetweenCalls / 1000);
+  callsCount = 0;
+  setTimeout(checkRate, checkRateMs);
+}
+checkRate();
+
+async function waitOrNot() {
+  if (waitBetweenCalls > 0) {
+    await new Promise(resolve => setTimeout(resolve, waitBetweenCalls));
+  }
+  callsCount++;
+}
+
 async function apiPost(path, data) {
   try {
+    await waitOrNot();
     const res = await superagent.post('https://api.copper.com/developer_api/v1/' + path).set(HEADERS).send(data);
     return res.body;
   } catch (e) {
-    throw new Error(e.message + ' ' + e.response?.text + ' ' + path);
+    throw new Error(e.message + ' ' + e.response?.text + ' ' + path + ' ' + JSON.stringify(data));
   }
 }
 
@@ -37,6 +95,7 @@ async function apiPostPaginated(path, data = {}) {
 async function apiGet(path, data = {}) {
   try {
     console.log(path);
+    await waitOrNot();
     const res = await superagent.get('https://api.copper.com/developer_api/v1/' + path).set(HEADERS).query(data);
     return res.body;
   } catch (e) {
@@ -44,19 +103,17 @@ async function apiGet(path, data = {}) {
   }
 }
 
+async function getSave(what) {
+  const path = 'data/' + what + '.json';
+  if (fs.existsSync(path)) return;
+  const item = await apiGet(what);
+  fs.writeFileSync(path, JSON.stringify(item, null, 2));
+}
+
 async function getItem(entityName) {
   if (fs.existsSync('data/' + entityName + 'List.json')) return;
   const entityList = await apiPostPaginated(entityName + '/search', {});
   fs.writeFileSync('data/' + entityName + 'List.json', JSON.stringify(entityList, null, 2));
-}
-
-const entityMap = {
-  'companies': 'company',
-  'people': 'person',
-  'opportunities': 'opportunity',
-  'projects': 'project',
-  'tasks': 'task',
-  'leads': 'lead'
 }
 
 async function getActivities(entityName) {
@@ -73,22 +130,16 @@ async function getActivities(entityName) {
   }
 }
 
-async function getSave(what) {
-  const path = 'data/' + what + '.json';
-  if (fs.existsSync(path)) return;
-  const item = await apiGet(what);
-  fs.writeFileSync(path, JSON.stringify(item, null, 2));
+async function getRelated(entityName, fileOrRelated) {
+  const entityList = require('./data/' + entityName + 'List.json');
+  const basePath = 'data/' + entityName  + '-' + fileOrRelated + '/';
+  fs.mkdirSync(basePath, { recursive: true });
+  const type = entityMap[entityName];
+
+  for (const entity of entityList) {
+    const path = basePath + entity.id + '.json';
+    if (fs.existsSync(path)) continue;
+    const related = await apiGet(entityName + '/' + entity.id + '/' + fileOrRelated);
+    fs.writeFileSync(path, JSON.stringify(related, null, 2));
+  }
 }
-
-
-(async () => {
-  for (const item of ['account', 'lead_statuses', 'contact_types', 'customer_sources', 'loss_reasons', 'pipelines', 'pipeline_stages', 'tags', 'activity_types', 'custom_activity_types', 'custom_field_definitions']) {
-    await getSave(item);
-  }
-  for (const item of Object.keys(entityMap)) {
-    await getItem(item);
-  }
-  for (const item of Object.keys(entityMap)) {
-    await getActivities(item);
-  }
-})();
